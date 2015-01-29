055916b352e5a3c6655371d48315f0e8512a4ba8
Morten <msalte86@gmail.com>
2015-01-29 20:14:16 +0100

    Moved torrent creation function into a separate utility class

--- bit-torrent/src/main/java/no/uis/msalte/thesis/bit_torrent/App.java
+++ bit-torrent/src/main/java/no/uis/msalte/thesis/bit_torrent/App.java
@@ -1,13 +1,6 @@
 package no.uis.msalte.thesis.bit_torrent;
 
 import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.net.URI;
-import java.util.ArrayList;
-import java.util.List;
-
-import com.turn.ttorrent.common.Torrent;
 
 public class App {
 	private static final String OWNER_DIR = "C:\\Users\\Morten\\Desktop\\ttorrent\\owner_dir";
@@ -22,37 +15,11 @@ public class App {
 			TORRENTS_DIR, TORRENT_FILE);
 
 	public static void main(String[] args) {
-//		createTorrent(OWNER_DIR, OWNER_FILE, TORRENTS_DIR, TORRENT_FILE);
+//		BitTorrentUtil.newTorrent(OWNER_DIR, OWNER_FILE, TORRENTS_DIR, TORRENT_FILE);
 
 		startTracker();
 		share(TORRENT_FULL_PATH, OWNER_DIR);
 		download(TORRENT_FULL_PATH, RECIPIENT_DIR);
-	}
-
-	private static void createTorrent(String sourceDir, String sourceFile,
-			String torrentDir, String torrentFile) {
-		File input = new File(String.format("%s\\%s", sourceDir, sourceFile));
-		File output = new File(String.format("%s\\%s", torrentDir, torrentFile));
-
-		URI announce = URI.create("http://localhost:6969/announce");
-
-		List<List<URI>> announceTiers = new ArrayList<List<URI>>();
-		List<URI> announceTier1 = new ArrayList<URI>();
-
-		announceTier1.add(announce);
-		announceTiers.add(announceTier1);
-
-		int pieceLength = 256 * 1000; // 256KB
-
-		try {
-			FileOutputStream fos = new FileOutputStream(output);
-			Torrent.create(input, pieceLength, announceTiers,
-					App.class.getPackage().getName()).save(fos);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
 	}
 
 	private static void share(String torrentPath, String outputDir) {
