85a0afc3f4e3aafc7d2d4ef8193c696977b939fa
Morten <msalte86@gmail.com>
2015-02-05 09:56:35 +0100

    Initial secure-cloud project commit

---
+++ secure-cloud/src/main/java/no/uis/msalte/thesis/secure_cloud/App.java
@@ -0,0 +1,13 @@
+package no.uis.msalte.thesis.secure_cloud;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
