4e59eca99373b681dfa77ba94fff5d59d3063605
Morten <msalte86@gmail.com>
2015-02-07 18:47:55 +0100

    Initial commit of web-service project

---
+++ web-service/src/main/java/no/uis/msalte/thesis/web_service/App.java
@@ -0,0 +1,13 @@
+package no.uis.msalte.thesis.web_service;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
