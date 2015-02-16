0ec14c68ce10e841ff61d3fb7f20f1ae8e4122e4
Morten <msalte86@gmail.com>
2015-02-16 16:40:54 +0100

    Formatting

--- secure-cloud/src/main/java/no/uis/msalte/thesis/secure_cloud/App.java
+++ secure-cloud/src/main/java/no/uis/msalte/thesis/secure_cloud/App.java
@@ -2,12 +2,11 @@ package no.uis.msalte.thesis.secure_cloud;
 
 import no.uis.msalte.thesis.secure_cloud.storage.Persist;
 
-
 public class App {
 	public static void main(String[] args) {
-		 Persist p = Persist.getInstance();
-		 
-		 System.out.println(p.formatMap(Persist.Maps.TORRENTS));
-		 System.out.println(p.formatMap(Persist.Maps.KEY_TUPLES));
+		Persist p = Persist.getInstance();
+
+		System.out.println(p.formatMap(Persist.Maps.TORRENTS));
+		System.out.println(p.formatMap(Persist.Maps.KEY_TUPLES));
 	}
 }
