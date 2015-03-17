703a9cf0335663d8e3caa4ef9430c86a8966aedf
Morten <msalte86@gmail.com>
2015-03-17 10:31:04 +0100

    Spark is now imported statically in Server

--- web-service/src/main/java/no/uis/msalte/thesis/web_service/server/Server.java
+++ web-service/src/main/java/no/uis/msalte/thesis/web_service/server/Server.java
@@ -13,13 +13,14 @@ import no.uis.msalte.thesis.web_service.routes.SharePostRoute;
 import no.uis.msalte.thesis.web_service.routes.UploadPostRoute;
 import no.uis.msalte.thesis.web_service.util.JsonTransformer;
 import no.uis.msalte.thesis.web_service.util.WebServiceUtils;
-import spark.Spark;
+
+import static spark.Spark.*;
 
 public class Server {
 
 	private static final String ACCEPT_TYPE = "application/json";
 	private static final JsonTransformer JSON_TRANSFORMER = new JsonTransformer();
-	
+
 	private static final String KEY_STORE_FILE = "C:\\Users\\Morten\\Desktop\\keystore\\web_service.jks";
 	private static final String KEY_STORE_PW = "esp8bing";
 
@@ -27,49 +28,48 @@ public class Server {
 
 	public void start() {
 		// ---- SETUP ---- //
-		Spark.port(HTTP_PORT);
-		Spark.secure(KEY_STORE_FILE, KEY_STORE_PW, null, null);
+		port(HTTP_PORT);
+		secure(KEY_STORE_FILE, KEY_STORE_PW, null, null);
 		setStaticFileLocation();
 
 		// ---- GET FUNCTIONS ---- //
-		Spark.get(NewSecretKeyGetRoute.PATH, ACCEPT_TYPE,
-				new NewSecretKeyGetRoute(), JSON_TRANSFORMER);
-
-		Spark.get(ApiGetRoute.PATH, ACCEPT_TYPE, new ApiGetRoute(),
+		get(NewSecretKeyGetRoute.PATH, ACCEPT_TYPE, new NewSecretKeyGetRoute(),
 				JSON_TRANSFORMER);
+
+		get(ApiGetRoute.PATH, ACCEPT_TYPE, new ApiGetRoute(), JSON_TRANSFORMER);
 
 		// ---- POST FUNCTIONS ---- //
-		Spark.post(UploadPostRoute.PATH, ACCEPT_TYPE, new UploadPostRoute(),
+		post(UploadPostRoute.PATH, ACCEPT_TYPE, new UploadPostRoute(),
 				JSON_TRANSFORMER);
 
-		Spark.post(NewPublicKeyPostRoute.PATH, ACCEPT_TYPE,
+		post(NewPublicKeyPostRoute.PATH, ACCEPT_TYPE,
 				new NewPublicKeyPostRoute(), JSON_TRANSFORMER);
 
-		Spark.post(NewReEncryptionKeyPostRoute.PATH, ACCEPT_TYPE,
+		post(NewReEncryptionKeyPostRoute.PATH, ACCEPT_TYPE,
 				new NewReEncryptionKeyPostRoute(), JSON_TRANSFORMER);
 
-		Spark.post(DecryptPostRoute.PATH, ACCEPT_TYPE, new DecryptPostRoute(),
+		post(DecryptPostRoute.PATH, ACCEPT_TYPE, new DecryptPostRoute(),
 				JSON_TRANSFORMER);
 
-		Spark.post(SharePostRoute.PATH, ACCEPT_TYPE, new SharePostRoute(),
+		post(SharePostRoute.PATH, ACCEPT_TYPE, new SharePostRoute(),
 				JSON_TRANSFORMER);
 
-		Spark.post(DownloadPostRoute.PATH, ACCEPT_TYPE,
-				new DownloadPostRoute(), JSON_TRANSFORMER);
+		post(DownloadPostRoute.PATH, ACCEPT_TYPE, new DownloadPostRoute(),
+				JSON_TRANSFORMER);
 
-		Spark.post(NewTorrentPostRoute.PATH, ACCEPT_TYPE,
-				new NewTorrentPostRoute(), JSON_TRANSFORMER);
+		post(NewTorrentPostRoute.PATH, ACCEPT_TYPE, new NewTorrentPostRoute(),
+				JSON_TRANSFORMER);
 	}
 
 	public void stop() {
-		Spark.stop();
+		stop();
 	}
 
 	private static void setStaticFileLocation() {
 		try {
 			final String location = WebServiceUtils
 					.getFileResource("script.js").getParent();
-			Spark.externalStaticFileLocation(location);
+			externalStaticFileLocation(location);
 		} catch (URISyntaxException e) {
 			e.printStackTrace();
 		}
